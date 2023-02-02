8d553c2a9fea3ee1f3b5dfcddc2dc902dd8fce4a
jarreed0 <jarreed0@gmail.com>
2023-02-02 09:49:28 -0600

    hide password

--- checker.py
+++ checker.py
@@ -6,6 +6,7 @@
 ## http://jar.ylimaf.com
 ## 2/1/2023
 
+import getpass
 from PwndPasswd import check
 import sys
 
@@ -14,6 +15,6 @@ passwd = ""
 if len(sys.argv) > 1:
    passwd = sys.argv[1]
 else:
-   passwd = input("Type your password: ")
+   passwd = getpass.getpass(prompt='Type your password: ')
 
 print("Your password was found {} times.".format(str(check(passwd))))
